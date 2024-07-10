7460fc0884f88b8c040e7f9e60dfb76b3ab2bb55
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-07-10 17:40:50 +0530

    updated with route path

--- server/src/routes/indexRoutes.js
+++ server/src/routes/indexRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import userRoutes from './userRoutes.js'
 
 const router = express.Router()
 
-router.use('/users',usersRoutes)
+router.use('/users',userRoutes)
 
 export default router
