45606d186588191af9463b42a0ef0d4e038498a9
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-07-11 00:49:35 +0530

    upadted with contact page content

--- client/src/components/contents/ContactContent.jsx
+++ client/src/components/contents/ContactContent.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
+import { toast } from 'react-toastify'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { Container, Col, Form, Button } from 'react-bootstrap'
+
 
 function ContactContent() {
-  return (
-    <div>ContactContent</div>
-  )
+
+  let formik = useFormik({
+    initialValues:{
+      name:'',
+      email:'',
+      mobile:'',
+      description:'',
+    },
+    validationSchema:Yup.object({       
+      name:Yup.string().required('Firstname is required').max(20,'Name can not exceed 20 characters').min(3,'firstName can not be shorter than 3 leters'),
+      email:Yup.string().required('Email is required').email('Enter a valid email'),
+      mobile:Yup.string().required('Mobile is required').matches(/^\d{10}$/,'Enter a valid mobile number'),
+      description:Yup.string().required('Description is required').min(10,'Description can not be shorter than 10 leters'),
+    }),
+    onSubmit : async(values) => {
+        try { 
+          console.log(values)       
+            // if(values.password === values.confirmPassword){
+            //   let res = await AxiosService.post(`${ApiRoutes.REGISTER.path}`,values)
+            //   if(res.status === 200){
+            //     navigate('/login')
+            //   }     
+            // }else{
+            //   toast.error("Passwords doesnt match! Please enter the same passwords")
+            // }
+        } catch (error) {
+            toast.error(error.response.data.message || error.message)
+        }
+    }
+  })
+
+  return <>
+    <Container className='contactBlock'>
+      <Col md xs={12} className='contactForm p-5'>
+        <Form onSubmit={formik.handleSubmit} className='contactQueryForm mx-auto rounded-5'>
+          
+          <Form.Group className="mb-4">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter Name" id='name' name='name' onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur}/>
+            {formik.touched.name && formik.errors.name ? (<div className='authErrorText'>{formik.errors.name}</div>) : null}
+          </Form.Group>
+          <Form.Group className="mb-4">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" id='email' name='email' onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur}/>
+            {formik.touched.email && formik.errors.email ? (<div className='authErrorText'>{formik.errors.email}</div>) : null}
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Mobile</Form.Label>
+            <Form.Control type="text" placeholder="Enter Mobile number" maxLength={10} id="mobile" name='mobile' onChange={formik.handleChange} value={formik.values.mobile} onBlur={formik.handleBlur}/>
+            {formik.touched.mobile && formik.errors.mobile ? (<div className='authErrorText'>{formik.errors.mobile}</div>) : null}
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder='Enter Query description' name='description' onChange={formik.handleChange} value={formik.values.description} onBlur={formik.handleBlur}/>
+            {formik.touched.description && formik.errors.description ? (<div className='authErrorText'>{formik.errors.description}</div>) : null}
+          </Form.Group>
+          <div className="d-grid mb-3"><Button variant='primary' className='formBtns' type="submit">Send Query</Button></div>
+        </Form>
+      </Col>
+
+      <div className='mapBlock p-5'>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62575.05133734738!2d76.65222171990958!3d11.411849727066063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba8bd84b5f3d78d%3A0x179bdb14c93e3f42!2sOoty%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1720637461973!5m2!1sen!2sin" width="600" height="450" loading="lazy" referrerpolicy="no-referrer-when-downgrade" allowFullScreen></iframe>
+      </div>
+    </Container>
+  </>
 }
 
 export default ContactContent
