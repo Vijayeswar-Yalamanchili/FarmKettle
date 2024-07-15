feeb0ff191b82934ae359cac98a87efac57e1126
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-07-15 20:21:58 +0530

    updated with my account page

---
+++ client/src/components/contents/MyAccountContent.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Card, Container, Row, Image } from 'react-bootstrap'
+import myProfileImage from '../../assets/myProfileImage.svg'
+import addressImage from '../../assets/addressImage.svg'
+import ordersImage from '../../assets/ordersImage.svg'
+
+function MyAccountContent() {
+
+    let accountCardsList = [
+        {
+            route : 'profile',
+            iconImg : myProfileImage,
+            title : 'My Profile',
+            subtitle : 'Modify your details'
+        },
+        {
+            route : 'address',
+            iconImg : addressImage,
+            title : 'Your Address',
+            subtitle : 'Delivery locations'
+        },
+        {
+            route : 'orders',
+            iconImg : ordersImage,
+            title : 'Your Orders',
+            subtitle : 'List of your orders'
+        }
+    ]
+
+    const handleCardBtn = async(e) => {
+        console.log(e.route)
+    }
+
+    return <>
+        <Container>
+            {/* <Row xs={1} md={2} className="g-4 my-4 mx-auto"> */}
+            <div className='accountCards'>
+                {
+                    accountCardsList.map((e,i) => (
+                        <Card className='cardAcc' key={i} onClick={() => handleCardBtn(e)}>
+                            <Card.Body className='cardBody'>
+                                <Image src={e.iconImg} className='cardImage'/>
+                                <div className='cardBodyText'>
+                                    <h4>{e.title}</h4>
+                                    <p className='subtitle'>{e.subtitle}</p>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    ))
+                }
+                </div>
+            {/* </Row> */}
+        </Container>
+    </>
+}
+
+export default MyAccountContent
--- client/src/pages/navs/MyAccount.jsx
+++ client/src/pages/navs/MyAccount.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AppNavbar from '../../components/AppNavbar'
 import AppFooter from '../../components/AppFooter'
-import AccountContent from '../../components/contents/AccountContent'
+// import AccountContent from '../../components/contents/AccountContent'
+import MyAccountContent from '../../components/contents/MyAccountContent'
 
 function MyAccount() {
     return <>
     <AppNavbar/>
-    <AccountContent/>
+    {/* <AccountContent/> */}
+    <MyAccountContent/>
     <AppFooter/>
     </>
 }
