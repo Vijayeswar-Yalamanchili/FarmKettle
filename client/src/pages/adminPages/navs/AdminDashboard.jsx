7a7cf7c12924dc8d022f75bdf5579fc4bfd41d71
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-07-18 23:07:42 +0530

    updated

--- client/src/pages/adminPages/navs/AdminDashboard.jsx
+++ client/src/pages/adminPages/navs/AdminDashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import AdminNavbar from '../../../components/adminComponents/AdminNavbar'
 import AdminFooter from '../../../components/adminComponents/AdminFooter'
+import AdminDashboardContent from '../../../components/adminComponents/content/AdminDashboardContent'
 
 function AdminDashboard() {
   return <>
     <AdminNavbar/>
-      
+    <AdminDashboardContent/>
     <AdminFooter/>
   </>
 }
