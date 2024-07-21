a7efaf73616d6788a283134c034ecec48c17ae5b
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-07-22 01:40:25 +0530

    updated lazy for all pages

--- client/src/utils/AppRoutes.jsx
+++ client/src/utils/AppRoutes.jsx
@@ -1,47 +1,56 @@
-import Login from "../pages/userPages/authentications/Login"
-import Register from "../pages/userPages/authentications/Register"
-import Home from "../pages/userPages/navs/Home"
-import About from "../pages/userPages/navs/About"
-import HealthCertifications from "../pages/userPages/navs/HealthCertifications"
-import Contact from "../pages/userPages/navs/Contact"
-import BuyProducts from "../pages/userPages/navs/BuyProducts"
-import MyAccount from "../pages/userPages/navs/MyAccount"
-import MyOrders from "../pages/userPages/navs/MyOrders"
-import MyProfile from "../pages/userPages/navs/MyProfile"
-import MyAddress from "../pages/userPages/navs/MyAddress"
-import AdminLogin from "../pages/adminPages/authentications/AdminLogin"
-import AdminRegister from "../pages/adminPages/authentications/AdminRegister"
-import AdminDashboard from "../pages/adminPages/navs/AdminDashboard"
-import AdminUsersList from "../pages/adminPages/navs/AdminUsersList"
-import AdminProductsList from "../pages/adminPages/navs/AdminProductsList"
-import AdminProfile from "../pages/adminPages/navs/AdminProfile"
-import UserListContextComponent from "../contextApi/UserListContextComponent"
-import Cart from "../pages/userPages/navs/Cart"
 import { lazy, Suspense } from "react"
 import LoadingComponent from '../components/userComponents/LoadingComponent'
+import UserListContextComponent from "../contextApi/UserListContextComponent"
 
+const LoginPage = lazy(()=> import('../pages/userPages/authentications/Login'))
+const RegisterPage = lazy(()=> import('../pages/userPages/authentications/Register'))
+const HomePage = lazy(()=> import('../pages/userPages/navs/Home'))
+const AboutPage = lazy(()=> import('../pages/userPages/navs/About'))
+const HCPage = lazy(()=> import('../pages/userPages/navs/HealthCertifications'))
+const ContactPage = lazy(()=> import('../pages/userPages/navs/Contact'))
 const Products = lazy(()=> import('../pages/userPages/navs/BuyProducts'))
 const CartPage = lazy(()=> import('../pages/userPages/navs/Cart'))
+const MyAccountPage = lazy(()=> import('../pages/userPages/navs/MyAccount'))
+const MyProfilePage = lazy(()=> import('../pages/userPages/navs/MyProfile'))
+const MyAddressPage = lazy(()=> import('../pages/userPages/navs/MyAddress'))
+const MyOrdersPage = lazy(()=> import('../pages/userPages/navs/MyOrders'))
+
+const AdminLoginPage = lazy(()=> import('../pages/adminPages/authentications/AdminLogin'))
+const AdminRegisterPage = lazy(()=> import('../pages/adminPages/authentications/AdminRegister'))
+const AdminProfilePage = lazy(()=> import('../pages/adminPages/navs/AdminProfile'))
+const AdminDashboardPage = lazy(()=> import('../pages/adminPages/navs/AdminDashboard'))
+const AdminUsersListPage = lazy(()=> import('../pages/adminPages/navs/AdminUsersList'))
+const AdminProductssListPage = lazy(()=> import('../pages/adminPages/navs/AdminProductsList'))
 
 const Approutes = [
     {
+        path : '/login',
+        element : <Suspense fallback={<LoadingComponent/>}><LoginPage/></Suspense>,
+        exact : true
+    },
+    {
+        path : '/register',
+        element : <Suspense fallback={<LoadingComponent/>}><RegisterPage/></Suspense>,
+        exact : true
+    },
+    {
         path : '/',
-        element : <Home/>,
+        element : <Suspense fallback={<LoadingComponent/>}><HomePage/></Suspense>,
         exact : true
     },
     {
         path : '/about',
-        element : <About/>,
+        element : <Suspense fallback={<LoadingComponent/>}><AboutPage/></Suspense>,
         exact : true
     },
     {
         path : '/healthCertifications',
-        element : <HealthCertifications/>,
+        element : <Suspense fallback={<LoadingComponent/>}><HCPage/></Suspense>,
         exact : true
     },
     {
         path : '/contact',
-        element : <Contact/>,
+        element : <Suspense fallback={<LoadingComponent/>}><ContactPage/></Suspense>,
         exact : true
     },
     {
@@ -50,70 +59,59 @@ const Approutes = [
         exact : true
     },
     {
-        path : '/login',
-        element : <Login/>,
-        exact : true
-    },
-    {
-        path : '/register',
-        element : <Register/>,
-        exact : true
-    },
-    {
-        path : '/myaccount',
-        element : <MyAccount/>,
-        exact : true
-    },
-    {
-        path : '/myaccount/profile',
-        element : <MyProfile/>,
-        exact : true
-    },
-    {
-        path : '/myaccount/address',
-        element : <MyAddress/>,
-        exact : true
-    },
-    {
         path : '/cart',
-        // element : <Cart/>,
         element : <Suspense fallback={<LoadingComponent/>}><CartPage/></Suspense>,
         exact : true
     },
     {
+        path : '/myaccount',
+        element : <Suspense fallback={<LoadingComponent/>}><MyAccountPage/></Suspense>,
+        exact : true
+    },
+    {
+        path : '/myaccount/profile',
+        element : <Suspense fallback={<LoadingComponent/>}><MyProfilePage/></Suspense>,
+        exact : true
+    },
+    {
+        path : '/myaccount/address',
+        element : <Suspense fallback={<LoadingComponent/>}><MyAddressPage/></Suspense>,
+        exact : true
+    },
+    {
         path : '/myorders',
-        element : <MyOrders/>,
+        element : <Suspense fallback={<LoadingComponent/>}><MyOrdersPage/></Suspense>,
         exact : true
     },
     // ADMIN
     {
         path : '/admin',
-        element : <AdminLogin/>,
+        element : <Suspense fallback={<LoadingComponent/>}><AdminLoginPage/></Suspense>,
         exact : true
     },
     {
         path : '/admin/register',
-        element : <AdminRegister/>,
+        element : <Suspense fallback={<LoadingComponent/>}><AdminRegisterPage/></Suspense>,
         exact : true
     },
     {
         path : '/admin/dashboard',
-        element : <UserListContextComponent><AdminDashboard/></UserListContextComponent>,
+        element : <Suspense fallback={<LoadingComponent/>}><UserListContextComponent><AdminDashboardPage/></UserListContextComponent></Suspense>,
         exact : true
     },
     {
         path : '/admin/dashboard/userslist',
-        element : <UserListContextComponent><AdminUsersList/></UserListContextComponent>,
+        element : <Suspense fallback={<LoadingComponent/>}><UserListContextComponent><AdminUsersListPage/></UserListContextComponent></Suspense>,
         exact : true
     },
     {
         path : '/admin/dashboard/productslist',
-        element : <UserListContextComponent><AdminProductsList/></UserListContextComponent>,
+        element : <Suspense fallback={<LoadingComponent/>}><UserListContextComponent><AdminProductssListPage/></UserListContextComponent></Suspense>,
         exact : true
     },
     {
         path : '/admin/adminprofile',
-        element : <AdminProfile/>,
+        element : <Suspense fallback={<LoadingComponent/>}><AdminProfilePage/></Suspense>,
         exact : true
     },
 ]
