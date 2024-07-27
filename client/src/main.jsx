1bf70928198a0c178a144d1d43ea75da4f435627
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-07-28 05:11:25 +0530

    updated with userauth context component removal

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,15 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 import CartDataComponent from './contextApi/CartDataComponent.jsx'
-import UserContextComponent from './contextApi/UserContextComponent.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <UserContextComponent>
         <CartDataComponent>
           <App/>
         </CartDataComponent>
-      </UserContextComponent>
     <ToastContainer autoClose={2000}/>
   </React.StrictMode>,
 )
